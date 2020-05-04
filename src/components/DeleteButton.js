4fd7443d9263428e37147dd8d71198d92c8f8647
Nikky Pyra <nikkypyra@outlook.com>
2020-05-04 14:35:16 +0200

    Add DeleteButton Component and Story

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function ButtonCancel({ onClick }) {
+  return (
+    <ImageWrapper onClick={onClick}>
+      <img src={process.env.PUBLIC_URL + '/images/delete.png'} alt="" />
+    </ImageWrapper>
+  )
+}
+
+const ImageWrapper = styled.div`
+  img {
+    height: 28px;
+    width: 28px;
+  }
+`
---
+++ src/components/DeleteButton.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DeleteButton from './DeleteButton'
+import { action } from '@storybook/addon-actions'
+import mobileWrapper from '../../.storybook/preview'
+
+export default {
+  title: 'Delete Button',
+  component: DeleteButton,
+  decorators: [mobileWrapper],
+}
+export const Button = () => <DeleteButton onClick={action('onClick')} />
