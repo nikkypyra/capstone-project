e3cbb50bf47a45ef4997f254ad60e0ce9a67dff9
Nikky Pyra <nikkypyra@outlook.com>
2020-05-06 13:16:52 +0200

    fix stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import GlobalStyles from '../src/GlobalStyles'
+import { MemoryRouter as Router } from 'react-router-dom'
 
 export default (renderComponent) => (
   <>
-    <GlobalStyles />
-    <div style={{ padding: 20, width: 400 }}>{renderComponent()}</div>
+    <Router>
+      <GlobalStyles />
+      <div style={{ padding: 20, width: 400 }}>{renderComponent()}</div>
+    </Router>
   </>
 )
--- src/components/Header.stories.js
+++ src/components/Header.stories.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from './Header'
+import mobileWrapper from '../../.storybook/preview'
 
 export default {
   title: 'Header',
   component: Header,
+  decorators: [mobileWrapper],
 }
 
 export const TestHeader = () => {
