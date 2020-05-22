8cc80cb704a9ec79de658433156c638c5d1e2615
Nikky Pyra <nikkypyra@outlook.com>
2020-05-22 22:50:55 +0200

    family style

--- src/components/settings/FamilyList.js
+++ src/components/settings/FamilyList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components/macro'
 import PropTypes from 'prop-types'
 import DeleteButton from '../buttons/DeleteButton'
+import userSrc from '../../images/user.png'
 
 FamilyList.propTypes = {
   user: PropTypes.object.isRequired,
@@ -19,8 +20,11 @@ export default function FamilyList({ user, allUsers, deleteFamily }) {
       {familyMembers.length !== 0 ? (
         familyMembers.map((person, index) => (
           <FamilyWrapper key={index}>
+            <Marker>
+              <img src={userSrc} alt="" />
+            </Marker>
             <div className="person">
-              <p>{person}</p>
+              <h4>{person}</h4>
             </div>
             <div className="delete">
               <DeleteButton onClick={() => deleteFamily(user, person)} />
@@ -34,15 +38,26 @@ export default function FamilyList({ user, allUsers, deleteFamily }) {
   )
 }
 
+const Title = styled.h2`
+  color: var(--primary);
+  margin-top: 20px;
+`
+
 const FamilyWrapper = styled.section`
   margin-top: 40px;
   display: grid;
   grid-template-rows: auto;
-  grid-template-columns: 5fr 1fr;
+  grid-template-columns: 1fr 4fr 1fr;
+  align-items: center;
 `
-const Title = styled.h2`
-  color: var(--primary);
-  margin-top: 20px;
+
+const Marker = styled.div`
+  img {
+    height: 40px;
+    width: 40px;
+    border: 4px solid var(--tertiary);
+    border-radius: 50%;
+  }
 `
 
 const Text = styled.p`
